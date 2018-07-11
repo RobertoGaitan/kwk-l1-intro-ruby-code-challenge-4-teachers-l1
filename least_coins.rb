2cee2ab354a519f963392c3fc4a9069359773b40
Roberto Gaitan <rgaitan.it@gmail.com>
2018-07-11 10:19:19 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,18 @@
-#write out your code here
 
-def least_coins(cents)
+def least_coins(change)
 
-#Code your answer here!
+coins_hash = Hash.new
 
+coins_hash[:quarters] = ((change/25).round)
+change = change - (25 * (change/25).round)
+
+coins_hash[:dimes] = ((change/10).round)
+change = change - (10 * (change/10).round)
+
+coins_hash[:nickels] = ((change/5).round)
+change = change - (5 * (change/5).round)
+
+coins_hash[:pennies] = change
+
+coins_hash
 end
